864727c6e51cefd1e5720a54e6fe3fe1cc8a9627
kunalkhedkar <kunalvkhedkar@gmail.com>
2018-11-20 11:48:03 +0530

    added new homeActivity

---
+++ app/src/main/java/com/example/kunalk/myapplication/views/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.example.kunalk.myapplication.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.kunalk.myapplication.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
